1ae814d9171af6f32cb56fc57b81f5841cbfacf3
Oden-Allfader <typdude@hotmail.com>
2016-09-21 11:28:11 +0200

    Update interpolation.cpp

--- interpolation.cpp
+++ interpolation.cpp
@@ -10,13 +10,13 @@ interpolation::evalLERP(glm::vec3 const& p0, glm::vec3 const& p1, float const x)
 
 glm::vec3
 interpolation::evalCatmullRom(glm::vec3 const& p0, glm::vec3 const& p1,
-                              glm::vec3 const& p2, glm::vec3 const& p3,
-                              float const t, float const x)
+	glm::vec3 const& p2, glm::vec3 const& p3,
+	float const t, float const x)
 {
 	//! \todo Implement this function
 	glm::vec3 px = p0*((-t)*x + 2.0f*t*pow(x, 2.0f) - t*pow(x, 3.0f))
 		+ p1*(1.0f + pow(x, 2.0f)*(t - 3) + pow(x, 3.0f)*(2 - t))
 		+ p2*(t*x + pow(x, 2.0f)*(3 - 2 * t) + pow(x, 3.0f)*(t - 2))
-		+ p3*(-t*pow(x, 2.0f) + pow(x ,3));
+		+ p3*(-t*pow(x, 2.0f) + t*pow(x, 3));
 	return px;
 }
