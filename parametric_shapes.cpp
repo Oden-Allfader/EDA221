6a91b6b550915af9a4d2b0f39a5d53e0ab698b75
Oden-Allfader <typdude@hotmail.com>
2016-10-03 17:20:32 +0200

    Update parametric_shapes.cpp

--- parametric_shapes.cpp
+++ parametric_shapes.cpp
@@ -10,20 +10,75 @@
 #include <vector>
 
 eda221::mesh_data
-parametric_shapes::createQuad(unsigned int width, unsigned int height)
+parametric_shapes::createQuad(unsigned int res_width, unsigned int res_height ,unsigned int width, unsigned int height)
 {
-	//! \todo Fill in the blanks
-	auto const vertices = std::array<glm::vec3, 4>{
-		glm::vec3(0.0f, 0.0f, 0.0f),
-			glm::vec3(static_cast<float>(width), 0.0f, 0.0f),
-			glm::vec3(static_cast<float>(width), static_cast<float>(height), 0.0f),
-			glm::vec3(0.0f, static_cast<float>(height), 0.0f)
-	};
+	auto const vertices_nb = res_width * res_height;
 
-	auto const indices = std::array<glm::uvec3, 2>{
-		glm::uvec3(0u, 1u, 2u),
-			glm::uvec3(0u, 2u, 3u)
-	};
+	//! \todo Fill in the blanks
+	auto vertices = std::vector<glm::vec3>(vertices_nb);
+	auto normals = std::vector<glm::vec3>(vertices_nb);
+	auto texcoords = std::vector<glm::vec3>(vertices_nb);
+	auto tangents = std::vector<glm::vec3>(vertices_nb);
+	auto binormals = std::vector<glm::vec3>(vertices_nb);
+
+	float w = 0.0f,
+		dw = width / (static_cast<float>(res_width) - 1.0f);
+	float h = 0.0f,
+		dh = height / (static_cast<float>(res_height) - 1.0f);
+	
+	size_t index = 0u;
+	for (unsigned int i = 0; i < res_height; i++) {
+		w = 0.0f;
+		for (unsigned int j = 0; j < res_width; j++) {
+			// vertex
+			vertices[index] = glm::vec3(w,0,h);
+
+			//texture coordinates
+			texcoords[index] = glm::vec3(static_cast<float>(j) / (static_cast<float>(res_width) - 1.0f),
+				static_cast<float>(i) / (static_cast<float>(res_height) - 1.0f),
+				0.0f);
+
+			// tangent
+			auto t = glm::vec3(0.0f, 0.0f, 1.0f);
+			t = glm::normalize(t);
+			tangents[index] = t;
+
+			// binormal
+			auto b = glm::vec3(1.0f, 0.0f, 0.0f);
+			b = glm::normalize(b);
+			binormals[index] = b;
+
+			// normal
+			auto const n = glm::cross(t, b);
+			normals[index] = n;
+			
+			++index;
+			w += dw;
+		}
+		h += dh;
+
+	}
+
+
+	auto indices = std::vector<glm::uvec3>(2u * (res_width - 1u) * (res_height - 1u));
+
+	index = 0u;
+	for (unsigned int i = 0u; i < res_height - 1u; ++i)
+	{
+		for (unsigned int j = 0u; j < res_width - 1u; ++j)
+		{
+			indices[index] = glm::uvec3(res_width * i + j,
+				res_width * i + j + 1u,
+				res_width * i + j + 1u + res_width);
+			++index;
+
+			indices[index] = glm::uvec3(res_width * i + j,
+				res_width * i + j + res_width + 1u,
+				res_width * i + j + res_width);
+			++index;
+		}
+	}
+
 
 	eda221::mesh_data data;
 
@@ -64,58 +119,54 @@ parametric_shapes::createQuad(unsigned int width, unsigned int height)
 
 	auto const vertices_offset = 0u;
 	auto const vertices_size = static_cast<GLsizeiptr>(vertices.size() * sizeof(glm::vec3));
-	auto const bo_size = static_cast<GLsizeiptr>(vertices_size);
+	auto const normals_offset = vertices_size;
+	auto const normals_size = static_cast<GLsizeiptr>(normals.size() * sizeof(glm::vec3));
+	auto const texcoords_offset = normals_offset + normals_size;
+	auto const texcoords_size = static_cast<GLsizeiptr>(texcoords.size() * sizeof(glm::vec3));
+	auto const tangents_offset = texcoords_offset + texcoords_size;
+	auto const tangents_size = static_cast<GLsizeiptr>(tangents.size() * sizeof(glm::vec3));
+	auto const binormals_offset = tangents_offset + tangents_size;
+	auto const binormals_size = static_cast<GLsizeiptr>(binormals.size() * sizeof(glm::vec3));
+	auto const bo_size = static_cast<GLsizeiptr>(vertices_size
+		+ normals_size
+		+ texcoords_size
+		+ tangents_size
+		+ binormals_size
+		);
 
+	glBufferData(GL_ARRAY_BUFFER, bo_size, nullptr, GL_STATIC_DRAW);
 
-	glBufferData(GL_ARRAY_BUFFER, bo_size,
-		/* where is the data stored on the CPU? */vertices.data(),
-		/* inform OpenGL that the data is modified once, but used often */GL_STATIC_DRAW);
-
-	// Vertices have been just stored into a buffer, but we still need to
-	// tell Vertex Array where to find them, and how to interpret the data
-	// within that buffer.
-	//
-	// You will see shaders in more detail in lab 3, but for now they are
-	// just pieces of code running on the GPU and responsible for moving
-	// all the vertices to clip space, and assigning a colour to each pixel
-	// covered by geometry.
-	// Those shaders have inputs, some of them are the data we just stored
-	// in a buffer object. We need to tell the Vertex Array which inputs
-	// are enabled, and this is done by the following line of code, which
-	// enables the input for vertices:
+	glBufferSubData(GL_ARRAY_BUFFER, vertices_offset, vertices_size, static_cast<GLvoid const*>(vertices.data()));
 	glEnableVertexAttribArray(static_cast<unsigned int>(eda221::shader_bindings::vertices));
+	glVertexAttribPointer(static_cast<unsigned int>(eda221::shader_bindings::vertices), 3, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<GLvoid const*>(0x0));
 
-	// Once an input is enabled, we need to explain where the data comes
-	// from, and how it interpret it. When calling the following function,
-	// the Vertex Array will automatically use the current buffer bound to
-	// GL_ARRAY_BUFFER as its source for the data. How to interpret it is
-	// specified below:
-	glVertexAttribPointer(static_cast<unsigned int>(eda221::shader_bindings::vertices),
-		/*! \todo how many components do our vertices have? */3,
-		/* what is the type of each component? */GL_FLOAT,
-		/* should it automatically normalise the values stored */GL_FALSE,
-		/* once all components of a vertex have been read, how far away (in bytes) is the next vertex? */0,
-		/* how far away (in bytes) from the start of the buffer is the first vertex? */reinterpret_cast<GLvoid const*>(0x0));
+	glBufferSubData(GL_ARRAY_BUFFER, normals_offset, normals_size, static_cast<GLvoid const*>(normals.data()));
+	glEnableVertexAttribArray(static_cast<unsigned int>(eda221::shader_bindings::normals));
+	glVertexAttribPointer(static_cast<unsigned int>(eda221::shader_bindings::normals), 3, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<GLvoid const*>(normals_offset));
 
-	// Now, let's allocate a second one for the indices.
-	//
-	// Have the buffer's name stored into `data.ibo`.
-	glGenBuffers(1, &data.ibo);
-	assert(data.ibo != 0u);
-	// We still want a 1D-array, but this time it should be a 1D-array of
-	// elements, aka. indices!
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, data.ibo);
+	glBufferSubData(GL_ARRAY_BUFFER, texcoords_offset, texcoords_size, static_cast<GLvoid const*>(texcoords.data()));
+	glEnableVertexAttribArray(static_cast<unsigned int>(eda221::shader_bindings::texcoords));
+	glVertexAttribPointer(static_cast<unsigned int>(eda221::shader_bindings::texcoords), 3, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<GLvoid const*>(texcoords_offset));
 
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, /*! \todo how many bytes should the buffer contain? */ static_cast<GLsizeiptr>(indices.size() * sizeof(glm::uvec3)),
-		/* where is the data stored on the CPU? */indices.data(),
-		/* inform OpenGL that the data is modified once, but used often */GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, tangents_offset, tangents_size, static_cast<GLvoid const*>(tangents.data()));
+	glEnableVertexAttribArray(static_cast<unsigned int>(eda221::shader_bindings::tangents));
+	glVertexAttribPointer(static_cast<unsigned int>(eda221::shader_bindings::tangents), 3, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<GLvoid const*>(tangents_offset));
+
+	glBufferSubData(GL_ARRAY_BUFFER, binormals_offset, binormals_size, static_cast<GLvoid const*>(binormals.data()));
+	glEnableVertexAttribArray(static_cast<unsigned int>(eda221::shader_bindings::binormals));
+	glVertexAttribPointer(static_cast<unsigned int>(eda221::shader_bindings::binormals), 3, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<GLvoid const*>(binormals_offset));
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0u);
 
 	data.indices_nb = indices.size() * 3u;
+	glGenBuffers(1, &data.ibo);
+	assert(data.ibo != 0u);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, data.ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<GLsizeiptr>(indices.size() * sizeof(glm::uvec3)), reinterpret_cast<GLvoid const*>(indices.data()), GL_STATIC_DRAW);
 
-	// All the data has been recorded, we can unbind them.
 	glBindVertexArray(0u);
-	glBindBuffer(GL_ARRAY_BUFFER, 0u);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0u);
+
 
 	return data;
 }
@@ -155,11 +206,11 @@ parametric_shapes::createSphere(unsigned int const res_theta,
 				radius*cos_theta*sin_phi);
 
 			//texture coordinates
-			texcoords[index] = glm::vec3(static_cast<float>(i) / (static_cast<float>(res_theta) - 1.0f),static_cast<float>(j) / (static_cast<float>(res_phi) - 1.0f),
+			texcoords[index] = glm::vec3(static_cast<float>(i) / (static_cast<float>(res_theta) - 1.0f), static_cast<float>(j) / (static_cast<float>(res_phi) - 1.0f),
 				0.0f);
 
 			// tangent
-			auto t = glm::vec3(radius*cos_theta*(sin_phi+0.01f), 0.0f, -radius*sin_theta*(sin_phi+0.01f));
+			auto t = glm::vec3(radius*cos_theta*(sin_phi + 0.01f), 0.0f, -radius*sin_theta*(sin_phi + 0.01f));
 			t = glm::normalize(t);
 			tangents[index] = t;
 
